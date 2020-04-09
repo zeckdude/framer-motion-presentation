58a7cf734d661e5755343e036f8a7d4b89b19a82
zeckdude <zeckdude@gmail.com>
2020-04-09 11:51:12 -0700

    1 file modified

--- Demo.js
+++ Demo.js
@@ -2,28 +2,36 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import styled from '@emotion/styled'
 
+const MotionBox = styled(motion.div)`
+  background-color: #4682b4;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+  font-weight: bold;
+`
+
+// https://www.framer.com/api/motion/animation/#variants
+
+const variants = {
+  start: {
+    x: 0,
+    rotate: 0,
+  },
+  finish: {
+    backgroundColor: '#ffc0cb',
+    x: 400,
+    rotate: 360,
+  }
+};
+
 const Demo = () => {
   return (
-    <motion.div
-      style={{
-        backgroundColor: '#4682b4',
-        width: '100px',
-        height: '100px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: '#ffffff',
-        fontWeight: 'bold'
-      }}
-      initial={{
-        x: 0,
-        rotate: 0,
-      }}
-      animate={{
-        backgroundColor: '#ffc0cb',
-        x: 400,
-        rotate: 360,
-      }}
+    <MotionBox
+      initial="start"
+      animate="finish"
       transition={{
         ease: 'easeInOut',
         delay: 2,
@@ -31,7 +39,7 @@ const Demo = () => {
       }}
     >
       Woah
-    </motion.div>
+    </MotionBox>
   );
 }
 
